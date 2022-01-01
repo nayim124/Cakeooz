10a28d5570691d7afe5af165ca7bf32dcc9631e0
Anwar Abdi <anwar.abdi@sky.uk>
2022-01-01 18:21:06 +0000

    added royal delights

--- public_html/home.php
+++ public_html/home.php
@@ -48,22 +48,28 @@ include "afterheader.php";
 					?></p>
 					
 					<div class="cards">
-					<div class="card-img" ><img src="./menuImages/RegularDelights/black-forest.png" id="black-forest" alt="black-forest"/></div>
-
-<div class="card-img"><img src="./menuImages/RegularDelights/creme-delight.png" id="creme-delight" alt="creme-delight"/></div>
-
-<div class="card-img" ><img src="./menuImages/RegularDelights/crescent-delight.png" id="crescent-delight" alt="crescent-delight"/></div>
-
-<div class="card-img" ><img src="./menuImages/RegularDelights/crown-delight.png" id="crown-delight" alt="crown-delight"/></div>
-
-<div class="card-img" ><img src="./menuImages/RegularDelights/crown-ooz.png" id="crown-ooz" alt="crown-ooz"/></div>
-
-<div class="card-img" ><img src="./menuImages/RegularDelights/eskimo-ooz.png" id="eskimo-ooz" alt="eskimo-ooz"/></div>
-
-<div class="card-img" ><img src="./menuImages/RegularDelights/floral-delight.png" id="floral-delight" alt="floral-delight"/></div>
-
-<div class="card-img" ><img src="./menuImages/RegularDelights/white-lady.png" id="white-lady" alt="white-lady"/></div>
-			</div>
+						<div class="regular-delights">
+							<div class="card-img" ><img src="./menuImages/RegularDelights/black-forest.png" id="black-forest" alt="black-forest"/></div>
+							<div class="card-img"><img src="./menuImages/RegularDelights/creme-delight.png" id="creme-delight" alt="creme-delight"/></div>
+							<div class="card-img" ><img src="./menuImages/RegularDelights/crescent-delight.png" id="crescent-delight" alt="crescent-delight"/></div>
+							<div class="card-img" ><img src="./menuImages/RegularDelights/crown-delight.png" id="crown-delight" alt="crown-delight"/></div>
+							<div class="card-img" ><img src="./menuImages/RegularDelights/crown-ooz.png" id="crown-ooz" alt="crown-ooz"/></div>
+							<div class="card-img" ><img src="./menuImages/RegularDelights/eskimo-ooz.png" id="eskimo-ooz" alt="eskimo-ooz"/></div>
+							<div class="card-img" ><img src="./menuImages/RegularDelights/floral-delight.png" id="floral-delight" alt="floral-delight"/></div>
+							<div class="card-img" ><img src="./menuImages/RegularDelights/white-lady.png" id="white-lady" alt="white-lady"/></div>
+						</div>
+						<br />
+						<div class="royal-delights">
+							<div class="card-img" ><img src="./menuImages/RoyalDelights/baby-shower-delight.png" id="baby-shower-delight" alt="baby-shower-delight"/></div>
+							<div class="card-img"><img src="./menuImages/RoyalDelights/castle-delight.png" id="castle-delight" alt="castle-delight"/></div>
+							<div class="card-img" ><img src="./menuImages/RoyalDelights/choco-ooz.png" id="choco-ooz" alt="choco-ooz"/></div>
+							<div class="card-img" ><img src="./menuImages/RoyalDelights/choco-truffle-trouble.png" id="choco-truffle-trouble" alt="choco-truffle-trouble"/></div>
+							<div class="card-img" ><img src="./menuImages/RoyalDelights/double-delight.png" id="double-delight" alt="double-delight"/></div>
+							<div class="card-img" ><img src="./menuImages/RoyalDelights/minion-ooz.png" id="minion-ooz" alt="minion-ooz"/></div>
+							<div class="card-img" ><img src="./menuImages/RoyalDelights/princess-delight.png" id="princess-delight" alt="fprincess-delight"/></div>
+							<div class="card-img" ><img src="./menuImages/RoyalDelights/santa-delight.png" id="santa-delight" alt="santa-delight"/></div>
+						</div>
+					</div>
 			 </div>
             </div>
         </div>
